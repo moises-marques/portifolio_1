8cf7b8bdf78504e737f4553380e26de1497a210a
Moisés Marques Pereira <mpm10433@gmail.com>
2025-09-14 22:14:59 -0300

    feat: ajusta estilos

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const btn = document.querySelector(".toggle-theme-btn");
+    btn.addEventListener("click", () => {
+        document.body.classList.toggle("dark-mode");
+    });
+});
